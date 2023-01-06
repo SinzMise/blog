61cd08eb92b2e50f0b3c037f338c707766fabc2c
SinzMise <mail@sinzmise.cf>
2023-01-06 22:03:56 +0800

    Site updated: 2023-01-06 22:03:54

--- js/hello.js
+++ js/hello.js
@@ -6,14 +6,14 @@ if (window.console) {
   };
   let string = function () {
     /*
- __ _                  _          _                                 
-/ _(_)_ __  ____ /\/\ (_)___  ___( )__    /\  /\___  _ __ ___   ___ 
-\ \| | '_ \|_  //    \| / __|/ _ \/ __|  / /_/ / _ \| '_ ` _ \ / _ \
-_\ \ | | | |/ // /\/\ \ \__ \  __/\__ \ / __  / (_) | | | | | |  __/
-\__/_|_| |_/___\/    \/_|___/\___||___/ \/ /_/ \___/|_| |_| |_|\___|
-Welcome to SinzMise's Home!
+ __ _                  _          _      _                          
+/ _(_)_ __  ____ /\/\ (_)___  ___( )__  | |__   ___  _   _ ___  ___ 
+\ \| | '_ \|_  //    \| / __|/ _ \/ __| | '_ \ / _ \| | | / __|/ _ \
+_\ \ | | | |/ // /\/\ \ \__ \  __/\__ \ | | | | (_) | |_| \__ \  __/
+\__/_|_| |_/___\/    \/_|___/\___||___/ |_| |_|\___/ \__,_|___/\___|
+Welcome to SinzMise's house!
     */
   };
-  console.log("\n%c Hexo 6.3.0 %c Butterfly 4.5.1 ","color:#444;background:#eee;padding:5px 0;","color:#F8F8FF;background:#F4A7B9;padding:5px 0;");
+  console.log("\n%c Hexo 6.3.0 %c Butterfly 4.5.1 ","color:#444;background:#eee;padding:5px 0;","color:#F8F8FF;background:#2d402c;padding:5px 0;");
   console.log(string.makeMulti());
 }
